527ad09b061f73306a1285ebc5105664d7065a91
Alexandre Perseval <alexandre.perseval@icloud.com>
2021-12-10 12:59:27 +0100

    Fix css

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const CONFIG = {
     //ip: "localhost",
-    //ip: "15.188.56.39",
-    ip: "ec2-15-188-56-39.eu-west-3.compute.amazonaws.com",
+    ip: "15.188.56.39",
+    //ip: "ec2-15-188-56-39.eu-west-3.compute.amazonaws.com",
     port: "8080",
 } 
